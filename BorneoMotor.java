3b0b6541ce135bafe84d1482a8b25b2fd2ac0eaa
ahmad arief <arief.ahmad@gmail.com>
2015-01-16 11:09:07 +0800

    Create BorneoMotor.java

---
+++ BorneoMotor.java
@@ -0,0 +1,25 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           /*
+ * Ini adalah file utama, atau file yang pertama kali dijalankan oleh netbeans.
+ * File ini disebut juga sebagai Class Utama atau Main Class.
+ * Jadi jaman dulu kita sebut file, saat ini disebut Class atau Object. :-)
+ * Kadang-kadang yang dimaksud Class itu juga ternyata adalah prosedur atau fungsi.
+ * Hehehe. Lucu juga. Kalau anda yang jago JAVA menjelaskan apa itu Class pada saya dipastikan saya tidak akan paham.
+ * Setelah membuat program ini, maka saya bisa menyimpulkan apa itu class. Ya yang di atas itu tadi... :-)
+ */
+package borneomotor;
+
+/**
+ *
+ * @author Ahmad
+ */
+public class BorneoMotor {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new fUtama().show();
+        
+       //new FReportHargaItem().show();
+    }
+    
+}
